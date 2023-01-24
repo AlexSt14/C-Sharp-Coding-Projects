16e4b669a97176acbd35b06a5fd423a7571aed47
alex_ <alex_adinuta@yahoo.com>
2023-01-24 23:15:23 +0000

    Boolean Logic Assignment Submission

---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BooleanLogicAssignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI violation? Please answer with True or False");
+            bool violation = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets have you had?");
+            int speedTicket = Convert.ToInt32(Console.ReadLine());
+
+            bool qualify = age > 15 && violation == false && speedTicket < 3;
+            Console.WriteLine("Do you qualify for car insurance?");
+            Console.WriteLine(qualify);
+            Console.ReadLine();
+
+        }
+    }
+}
