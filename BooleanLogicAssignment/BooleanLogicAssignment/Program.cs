f8296fb32431b6bb620fd90b56d4bdf7b68ed780
alex_ <alex_adinuta@yahoo.com>
2023-01-24 23:17:23 +0000

    Adding comments

--- BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -8,13 +8,13 @@ namespace BooleanLogicAssignment
         {
             
             Console.WriteLine("What is your age?");
-            int age = Convert.ToInt32(Console.ReadLine());
+            int age = Convert.ToInt32(Console.ReadLine()); //reads input and converts to int
             Console.WriteLine("Have you ever had a DUI violation? Please answer with True or False");
-            bool violation = Convert.ToBoolean(Console.ReadLine());
+            bool violation = Convert.ToBoolean(Console.ReadLine()); //reads input and converts to boolean logic
             Console.WriteLine("How many speeding tickets have you had?");
-            int speedTicket = Convert.ToInt32(Console.ReadLine());
+            int speedTicket = Convert.ToInt32(Console.ReadLine()); //reads input and converts to int
 
-            bool qualify = age > 15 && violation == false && speedTicket < 3;
+            bool qualify = age > 15 && violation == false && speedTicket < 3; //logic based on answers to determine if qualify 
             Console.WriteLine("Do you qualify for car insurance?");
             Console.WriteLine(qualify);
             Console.ReadLine();
