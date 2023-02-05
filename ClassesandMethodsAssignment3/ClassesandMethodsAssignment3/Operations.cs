d2fda8b14b5ae52fd622cd8abcc2a0abb6f9f118
alex_ <alex_adinuta@yahoo.com>
2023-02-05 23:59:38 +0000

    Classes and Methods Assignment 3

---
+++ ClassesandMethodsAssignment3/ClassesandMethodsAssignment3/Operations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesandMethodsAssignment3
+{
+    internal class Operations
+    {
+        public void Operation(int numberOne, int numberTwo)
+        {
+            int operation = numberOne * 5;
+            Console.WriteLine(numberTwo);
+        }
+    }
+}
---
+++ ClassesandMethodsAssignment3/ClassesandMethodsAssignment3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesandMethodsAssignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations operations = new Operations();
+            int a = 10;
+            int b = 2;
+            Console.WriteLine(operations.Operation(a, b));
+            Console.ReadLine();
+        }
+    }
+}
