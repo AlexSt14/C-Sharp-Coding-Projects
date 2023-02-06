0b0e420543f581b77733c5c77b41bd94b0a5bcc9
alex_ <alex_adinuta@yahoo.com>
2023-02-06 00:45:05 +0000

    Class Method Assignment

---
+++ ClassMethod/ClassMethod/ClassAssignment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    static class ClassAssignment
+    {
+        public static void Operations(int a)        //void method, no return value, to write on screen we need to add here the writeline command
+        {
+            int result = a / 2;
+            Console.WriteLine(result);
+        }
+        public static int Operations2(int a, int b)     //method with output parameters
+        {
+            return a % b;
+        }
+        public static int Operations2(string a)     //overloaded method with a different operation
+        {
+            int result = Convert.ToInt32(a) * 200;            
+            return result;
+        }
+    }
+}
---
+++ ClassMethod/ClassMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Please choose a number to be divided by 2");
+            int input = Convert.ToInt32(Console.ReadLine());
+            ClassAssignment.Operations(input);          //calling in the class method while class is static, no need to instantiate the class, called the method which will divide the number by 2 and display the result
+
+            int result = ClassAssignment.Operations2(2, input);     //calling in the class method while class is static, no need to instantiate the class
+            Console.WriteLine(result);
+
+            int result2 = ClassAssignment.Operations2("2");     //calling in the class method while class is static, no need to instantiate the class
+            Console.WriteLine(result2);
+            Console.ReadLine();
+        }
+    }
+}
