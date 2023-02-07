aed49494e34e56b02a69cb5c4418c247414ae0b7
alex_ <alex_adinuta@yahoo.com>
2023-02-07 00:32:28 +0000

    Generics Assignment

---
+++ GenericsAssignment/GenericsAssignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    internal class Employee<T>
+    {
+        public List<T> things { get; set; }     //Assigning a parameter with a generic data type
+    }
+}
---
+++ GenericsAssignment/GenericsAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();         //Instantiating an employee object with type string as its generic parameter
+            employee.things = new List<string>() { "Alex", "Daniel", "Matthew" };       //Firstly creating a list, then assigning some strings as the property value of things
+            
+            foreach (string thing in employee.things)       //looping through the list to show on screen what list contains
+            {
+                Console.WriteLine(thing);
+            }
+
+            Employee<int> employee1 = new Employee<int>();      //Instantiating an employee object with type int as its generic paramenter
+            employee1.things = new List<int>() { 1, 5, 3978, 123, 25 };     //Firstly creting a list, then assigning some integers as the property value of things
+
+            foreach (int thing in employee1.things)     //looping through the list to show on screen what list contains
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+    }
+}
