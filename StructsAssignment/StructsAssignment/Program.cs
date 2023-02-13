4cdd97da9512708e36a94f7ac8888ec2b4cda7e4
alex_ <alex_adinuta@yahoo.com>
2023-02-13 14:48:27 +0000

    Structs Assignment

---
+++ StructsAssignment/StructsAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Number number = new Number();
+            number.Amount = 20.213m;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
