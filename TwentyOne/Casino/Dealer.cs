33acfd1124c389b0478505dc7cd84e588394b3eb
alex_ <alex_adinuta@yahoo.com>
2023-02-19 14:45:16 +0000

    Twenty One (BlackJack) Console Game Project

---
+++ TwentyOne/Casino/Dealer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Casino
+{
+    public class Dealer     //Dealer parent class
+    {
+        public string Name { get; set; }    //Props for dealer
+        public Deck Deck { get; set; }
+        public int Balance { get; set; }
+
+        public void Deal(List<Card> Hand)       //Deal method that is called in play method of games, passing in as paramenter a list of cards Hand
+        {
+            Hand.Add(Deck.Cards.First());       //Adding to that list of cards Hand, the first card in the deck
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");      //the string card will be logged to a text file every time the dealer is dealing a card
+            Console.WriteLine(card);    //will display that first card to the console
+            using (StreamWriter file = new StreamWriter(@"F:\Web Developer\log.txt", true))     //using the "using" before, we make sure the memory gets cleaned up after we are done, creating a new streamwriter object, taking in the path where the text file is and takes another 
+            {                                                                                   //argument "true" where it says "yes, I do want to append to the log file"
+                file.Write(DateTime.Now);       //Logging the date and time of each card that the dealer has given to the player
+                file.WriteLine(card);       //By using this, we will write the card string to the text file, the card string will be the card that the dealer is dealing
+            }
+                Deck.Cards.RemoveAt(0);     //Removing that card from the original deck as its now in the player's hand
+        }
+    }
+}
---
+++ TwentyOne/Casino/Deck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public class Deck
+    {
+        public Deck()   //This is a constructor
+        {
+            Cards = new List<Card>();
+
+            for (int i = 0; i < 13; i++)        //Utilizing enums and creating a deck of cards based on that
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Card card = new Card();
+                    card.Face = (Face)i;
+                    card.Suit = (Suit)j;
+                    Cards.Add(card);
+                }
+            }
+        }
+        public List<Card> Cards { get; set; }       //This is property of the class
+        public void Shuffle(int times = 1)  //creating an optional parameter "times" apart from the deck parameter, to be able to shuffle more times if a parameter is given
+        {                
+            for (int i = 0; i < times; i++)     //shuffle method for the deck of cards which takes in the times parameter to establish how many times will this loop go for
+            {                
+                List<Card> tempList = new List<Card>();     //creating a new list of cards which is temporary for this loop
+                Random random = new Random();       //creating a random number
+
+                while (Cards.Count > 0)     //this loop will go while there are cards in the Cards list
+                {
+                    int randomIndex = random.Next(0, Cards.Count);      //Assigning a random number to the index between 0 and the count of the Cards list
+                    tempList.Add(Cards[randomIndex]);       //Using that number as an index to add the card at that index to the new list
+                    Cards.RemoveAt(randomIndex);        //Using that number as an index to remove the card from the Card list
+                }
+                Cards = tempList;       //Once there are no more cards in the Cards list, we will assign the tempList to the Cards list
+            }                 
+        }
+    }
+}
