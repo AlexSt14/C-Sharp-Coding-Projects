b5ceab9dedff2eea43b9fdb793dd18fec51d7265
alex_ <alex_adinuta@yahoo.com>
2023-02-20 01:18:50 +0000

    Additional Features Assignment

---
+++ AdditionalFeatures/AdditionalFeatures/AdditionalFeaturesAssignment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures
+{
+    internal class AdditionalFeaturesAssignment
+    {
+        public AdditionalFeaturesAssignment(string name) : this(name, "City unknown") { }       //Chaining a second construct that inherits from the first
+        public AdditionalFeaturesAssignment(string name, string city)       //Creating a construct of the class that takes in 2 parameters
+        {
+            myName = name;
+            myCity = city;
+        }
+
+        public string myName { get; set; }
+        public string myCity { get; set; }
+    }
+}
---
+++ AdditionalFeatures/AdditionalFeatures/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Daniel Alex Sthali";       //Creating a constant as my name will never change
+            var myCity = "Chorley";     //Creating a variable using var instead of typing the exact data type
+            Console.WriteLine("My name is {0} and I live in {1}.", name, myCity);
+            Console.WriteLine("What is your name?");
+            string answer = Console.ReadLine();
+            Console.WriteLine("And where do you live?");
+            string answer2 = Console.ReadLine();            
+            
+            AdditionalFeaturesAssignment personDetails = new AdditionalFeaturesAssignment(answer, answer2);      //Instantiating an object and passing in as parameters the answer from the user
+
+            Console.WriteLine("Your name is {0} and you live in {1}", personDetails.myName, personDetails.myCity);      //Writing back to the console the answers
+
+            Console.ReadLine();
+
+        }
+    }
+}
